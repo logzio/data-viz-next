ff3942ce27cf660903d9d41395b20b762214c0ae
copyhold <ilya.novojilov@logz.io>
2024-03-27 17:10:35 +0200

    make logz services work

--- packages/grafana-data/src/utils/logzioProvider.ts
+++ packages/grafana-data/src/utils/logzioProvider.ts
@@ -1,17 +1,8 @@
 // LOGZ.IO GRAFANA CHANGE
-export let logzioDeprecatedAngularServices: any = {};
-export let logzioServices: any = {};
-export let logzioConfigs: any = {};
-export let productLoaded: any = {};
-
-if ((global as any).window) {
-  const logzioConfigurations = (window as any).logzio;
-  if (logzioConfigurations) {
-    logzioConfigs = logzioConfigurations.configs || {};
-    logzioDeprecatedAngularServices = logzioConfigurations.services || {};
-    productLoaded = logzioConfigurations.productLoaded || {};
-    if ((global as any).parent.__logzio__) {
-      logzioServices = (global as any).parent.__logzio__.services || {};
-    }
+export let logzioDeprecatedAngularServices: any = (global as any).parent.__logzio__.services;
+export let logzioServices: any = (global as any).parent.__logzio__.services;
+export const logzioConfigs = new Proxy<any>({}, {
+  get(target, prop) {
+    return (window as any).logzio?.configs[prop];
   }
-}
+})
