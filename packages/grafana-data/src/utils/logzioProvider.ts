756f712eb120a50ef87723ac3f9d3074c55afece
copyhold <ilya.novojilov@logz.io>
2024-03-28 11:45:01 +0200

    Clean up

--- packages/grafana-data/src/utils/logzioProvider.ts
+++ packages/grafana-data/src/utils/logzioProvider.ts
@@ -1,8 +1,7 @@
 // LOGZ.IO GRAFANA CHANGE
-export let logzioDeprecatedAngularServices: any = (global as any).parent.__logzio__.services;
 export let logzioServices: any = (global as any).parent.__logzio__.services;
 export const logzioConfigs = new Proxy<any>({}, {
   get(target, prop) {
     return (window as any).logzio?.configs[prop];
   }
-})
+});
