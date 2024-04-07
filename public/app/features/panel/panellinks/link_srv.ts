f8398d7614fa8b2761594f1643d988dc8c47e070
copyhold <ilya.novojilov@logz.io>
2024-04-07 16:23:37 +0300

    DEV-28224 disable exemplars Disable sending frontend metrics Email validation Adding types icons Handle email endpoint Disable endpoints editing in grafana (ALERTS) Add emails input --> Disable AM Selector DEV-33551

--- public/app/features/panel/panellinks/link_srv.ts
+++ public/app/features/panel/panellinks/link_srv.ts
@@ -11,7 +11,7 @@ import {
   InterpolateFunction,
   KeyValue,
   LinkModel,
-  locationUtil,
+  // locationUtil, // LOGZ.IO GRAFANA CHANGE :: comment out to prevent ts errors
   ScopedVars,
   textUtil,
   urlUtil,
@@ -262,8 +262,10 @@ export class LinkSrv implements LinkService {
       params[`\$${DataLinkBuiltInVars.includeVars}`] = true;
     }
 
-    let url = locationUtil.assureBaseUrl(urlUtil.appendQueryToUrl(link.url || '', urlUtil.toUrlParams(params)));
-    url = getTemplateSrv().replace(url);
+    // LOGZ.IO GRAFANA CHANGE :: Disable adding appSubUrl (/grafana-app) to the url
+    // let url = locationUtil.assureBaseUrl(urlUtil.appendQueryToUrl(link.url || '', urlUtil.toUrlParams(params)));
+    let url = getTemplateSrv().replace(link.url || '');
+    // LOGZ.IO GRAFANA CHANGE :: END
 
     return getConfig().disableSanitizeHtml ? url : textUtil.sanitizeUrl(url);
   }
@@ -295,9 +297,15 @@ export class LinkSrv implements LinkService {
     }
 
     const info: LinkModel<T> = {
-      href: locationUtil.assureBaseUrl(href.replace(/\n/g, '')),
+      // LOGZ.IO GRAFANA CHANGE :: Do not remove spaces from data link
+      // href: locationUtil.assureBaseUrl(href.replace(/\n/g, '')),
+      href: href.replace(/\n/g, ''),
+      // LOGZ.IO GRAFANA CHANGE :: END
       title: link.title ?? '',
-      target: link.targetBlank ? '_blank' : undefined,
+      // LOGZ.IO GRAFANA CHANGE :: link open on same tab to open on top frame
+      // target: link.targetBlank ? '_blank' : undefined,
+      target: link.targetBlank ? '_blank' : '_top',
+      // LOGZ.IO GRAFANA CHANGE :: END
       origin,
     };
 
